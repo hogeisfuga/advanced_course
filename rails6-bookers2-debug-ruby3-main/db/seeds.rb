5cc3e91783adc4cbcd0628c868ab256daf2195b2
Akitoshi <69295776+hogeisfuga@users.noreply.github.com>
2023-05-21 06:15:34 +0000

    seedデータ作成

--- rails6-bookers2-debug-ruby3-main/db/seeds.rb
+++ rails6-bookers2-debug-ruby3-main/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+(1..5).each do |i|
+  User.create(name: "hoge#{i}", email: "hoge#{i}@example.com", password: "111111")
+  Book.create(title: "book#{i}", body: "body#{i}", user_id: "#{i}")
+end
+
+(1..5).each do |i|
+  BookComment.new(book_id: i, user_id: rand(1..5), comment: "{おすすめの書籍です}")
+end
+
+(1..5).each do |i|
+  Favorite.create(user_id: i, book_id: i)
+end
