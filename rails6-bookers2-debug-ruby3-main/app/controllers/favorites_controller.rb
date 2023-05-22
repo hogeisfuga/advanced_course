b1a7b0388c4bbede587ab82082e2a7d51abf66f8
Akitoshi <69295776+hogeisfuga@users.noreply.github.com>
2023-05-22 12:49:31 +0000

    お気に入りの不具合修正

--- rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
+++ rails6-bookers2-debug-ruby3-main/app/controllers/favorites_controller.rb
@@ -1,26 +1,38 @@
 class FavoritesController < ApplicationController
   def create
-    @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     if favorite.save
-      set_books
+      render_js_by_request
     end
   end
 
   def destroy
-    @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     if favorite.destroy
-      set_books
+      render_js_by_request
     end
   end
 
   private
 
-  def set_books
-    to = Time.zone.now.at_end_of_day
-    from = (to - 6.days).at_beginning_of_day
-    @books = Book.sort_by_favorites_count_desc(from, to)
+  def set_book
+    @book = Book.find(params[:book_id])
   end
 
+  def render_js_by_request
+    render 'renderFavorite.js.erb' if request.referer.blank?
+
+    case URI(request.referer.to_s).path
+      when %r(^\/users)
+        @books = current_user.books
+        return render 'renderBooks.js.erb'
+      when %r(^\/books$)
+        to = Time.zone.now.at_end_of_day
+        from = (to - 6.days).at_beginning_of_day
+        @books = Book.sort_by_favorites_count_desc(from, to)
+        return render 'renderBooks.js.erb'
+      when %r(^\/books\/\d)
+        return render 'renderFavorite.js.erb'
+    end
+  end
 end
